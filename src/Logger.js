bc48f270f970a8bcb61eb29bdc7984b0339ca704
Bartlomiej Pietrzyk <pietrzyk.bartlomiej@gmail.com>
2019-11-24 18:24:42 +0100

    logger

---
+++ src/Logger.js
@@ -0,0 +1,23 @@
+const chalk = require('chalk');
+const moment = require('moment');
+
+export class Logger {
+  constrcutor(appName) {
+    this.appName = (appName || '')
+      .toUpperCase()
+      .split(' ')
+      .join('_');
+  }
+
+  log(message, vars) {
+    const dateString = moment().format('DD/MM/YYYY HH:mm:ss');
+
+    if (typeof vars === 'string') {
+      message += ' ' + chalk.blue(vars);
+    } else if (Array.isArray(vars)) {
+      message += ' ' + vars.map(p => chalk.blue(p)).join(' | ');
+    }
+
+    console.log(chalk.blue(`[${this.appName}]`) + chalk.green(`[${dateString}]`) + `: ${message}`);
+  }
+}
